b4d48f4c70d4e337909627ae12d029d17a007dc4
Aleksandr Tsarev <a.tsarev@haulmont.com>
2021-03-15 15:25:48 +0400

    Finish QuickStart

--- src/main/java/com/company/planner/screen/talk/TalkBrowse.java
+++ src/main/java/com/company/planner/screen/talk/TalkBrowse.java
@@ -1,10 +1,43 @@
 package com.company.planner.screen.talk;
 
+import com.company.planner.service.TalksScheduleService;
+import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.component.Calendar;
+import io.jmix.ui.component.VBoxLayout;
 import io.jmix.ui.screen.*;
 import com.company.planner.entity.Talk;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Named;
+import java.time.LocalDateTime;
 
 @UiController("plnnr_Talk.browse")
 @UiDescriptor("talk-browse.xml")
 @LookupComponent("talksTable")
 public class TalkBrowse extends StandardLookup<Talk> {
+    @Named("talksTabSheet.calendarTab")
+    private VBoxLayout calendarTab;
+    @Autowired
+    private ScreenBuilders screenBuilders;
+    @Autowired
+    private TalksScheduleService talksScheduleService;
+
+    @Subscribe("talksCalendar")
+    public void onTalksCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
+        screenBuilders.editor(Talk.class, this)
+                .editEntity(((Talk) event.getEntity()))
+                .withOpenMode(OpenMode.DIALOG)
+                .withScreenClass(TalkEdit.class)
+                .withAfterCloseListener(afterCloseEvent -> {
+                    if (afterCloseEvent.closedWith(StandardOutcome.COMMIT)) {
+                        getScreenData().loadAll();
+                    }
+                }).show();
+    }
+
+    @Subscribe("talksCalendar")
+    public void onTalksCalendarCalendarEventMove(Calendar.CalendarEventMoveEvent<LocalDateTime> event) {
+        talksScheduleService.rescheduleTalk(((Talk) event.getEntity()), event.getNewStart());
+        getScreenData().loadAll();
+    }
 }
